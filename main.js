e78c626dcb632ba37ce13644ab2781854ae0d3b3
CarlosRR19 <carlosrr19@educastur.es>
2024-03-12 09:40:38 +0100

    Cambiar color

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ document.addEventListener("DOMContentLoaded", function () {
     parrafo.textContent = parrafo.textContent.toUpperCase();
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var botonCambiarColor = document.getElementById("cambiarColorBtn");
+  
+    botonCambiarColor.addEventListener("click", function () {
+      cambiarColor();
+    });
+  
+    function cambiarColor() {
+      var colorRojo = Math.floor(Math.random() * 256);
+      var colorVerde = Math.floor(Math.random() * 256);
+      var colorAzul = Math.floor(Math.random() * 256);
+  
+      document.body.style.backgroundColor =
+        "rgb(" + colorRojo + "," + colorVerde + "," + colorAzul + ")";
+    }
+  });
+  
